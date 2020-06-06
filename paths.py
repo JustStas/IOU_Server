a5640a4dcf7ed720708998b347ef97360b0cdc20
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-06 18:18:01 +0300

    Added databases

--- paths.py
+++ paths.py
@@ -1,4 +1,4 @@
-trx_log_path = 'D:/OneDrive/IOU/Storage/trx_log.h5'
-users_db_path = 'D:/OneDrive/IOU/Storage/users_db.h5'
+trx_log_path = '/IOU/Storage/trx_log.h5'
+users_db_path = '/IOU/Storage/users_db.h5'
 server_ip = ''
 server_port = 53210
