e5663885054e4aba33e788143194471771a0ef46
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-06 18:55:26 +0300

    Updated paths

--- paths.py
+++ paths.py
@@ -1,4 +1,4 @@
-trx_log_path = '/IOU/Storage/trx_log.h5'
-users_db_path = '/IOU/Storage/users_db.h5'
+trx_log_path = '/Storage/trx_log.h5'
+users_db_path = '/Storage/users_db.h5'
 server_ip = ''
 server_port = 53210
