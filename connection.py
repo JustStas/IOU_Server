3ae1a4bb2ab3a48fd8314fcbc80ee5354a6128d5
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-06 18:28:53 +0300

    Removed Russian

--- connection.py
+++ connection.py
@@ -4,22 +4,21 @@ from core import process_data
 from paths import server_port, server_ip
 
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-serv_sock.bind((server_ip, server_port)) # в кавычках - IP, второй параметр - порт
+serv_sock.bind((server_ip, server_port)) # IP, port
 serv_sock.listen(10)
 
 while True:
-    # Бесконечно обрабатываем входящие подключения
+    # Continuous work on inbound connections
     try: client_sock, client_addr = serv_sock.accept()
     except socket.error:
         pass
     print('Connected by', client_addr)
 
     while True:
-        # Пока клиент не отключился, читаем передаваемые
-        # им данные и отправляем их обратно
+        # Read untill client disconnects and send back
         input = client_sock.recv(1024)
         if not input:
-            # Клиент отключился
+            # Client disconnected
             break
         print('Received data')
         try:
