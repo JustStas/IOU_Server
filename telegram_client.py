fb9eb16561bc9c5ef97f3ac13627db73ca42dc4b
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-09 20:26:14 +0300

    Added dummy telebot

--- telegram_client.py
+++ telegram_client.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot('1012372350:AAG7N6oZPE5mi9uLSsNwwvN2fZhHEJRlNVk')
 
 print('Bot started')
 
-@bot.message_handler(content_types=['message'])
+@bot.message_handler(content_types=['text'])
 def reply(message):
     bot.reply_to(message, 'Das is reply')
 
