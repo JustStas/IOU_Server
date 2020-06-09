e33d25e06009c14b087478dda139b4ec26d7542d
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-09 20:21:15 +0300

    Added dummy telebot

---
+++ telegram_client.py
@@ -0,0 +1,12 @@
+import telebot
+
+bot = telebot.TeleBot('1012372350:AAG7N6oZPE5mi9uLSsNwwvN2fZhHEJRlNVk')
+
+print('Bot started')
+
+@bot.message_handler(content_types=['video'])
+def reply(message):
+    bot.reply_to(message, 'Das is reply')
+
+while True:
+    bot.polling()
