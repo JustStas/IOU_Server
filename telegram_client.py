d87081f819aa6137290eeb1fe20c44222fa000c5
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-11 22:14:44 +0300

    Testing force reply

--- telegram_client.py
+++ telegram_client.py
@@ -12,10 +12,10 @@ print('Bot started')
 @bot.message_handler(commands=['balance_overview'])
 def balance_overview(message):
     user_ids = -1
-
     markup = types.ForceReply(selective=False)
-    test = bot.reply_to(message, 'Who do you want the overview for?', markup=markup)
-    print(test)
+    bot.send_message(message.chat.id, 'Who do you want the overview for?', reply_markup=markup)
+    # test = bot.reply_to(message, 'Who do you want the overview for?',markup=markup)
+    # print(test)
 
     print('Telegram --> Balance overview')
     to_print = ''
