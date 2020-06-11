5b569500824820c13fddb9230ff19bd02f945b3d
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-11 23:59:42 +0300

    Testing buttons

--- telegram_client.py
+++ telegram_client.py
@@ -96,14 +96,15 @@ def balance_overview(message):
 
 @bot.message_handler(commands=['load_user'])
 def balance(message):
+    keyboard = types.InlineKeyboardMarkup()
     buttons = []
     user_ids = list_users()
     print(user_ids)
     for id in user_ids:
         print('id',id)
         buttons.append(types.InlineKeyboardButton(id, callback_data=id))
-    markup = types.InlineKeyboardMarkup(buttons)
-    user_ids = bot.send_message(message.chat.id, 'Who do you want to load?', reply_markup=markup)
+    keyboard.add(buttons)
+    user_ids = bot.send_message(message.chat.id, 'Who do you want to load?', reply_markup=keyboard)
     bot.register_for_reply(user_ids, load_user)
 
 def load_user(message):
