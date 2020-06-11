c2fb6a000b31701833d43fef1f8f52268d78fa8c
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-11 23:08:37 +0300

    Testing force reply

--- support_functions.py
+++ support_functions.py
@@ -1,9 +1,5 @@
 import ast
 
 def str_to_list(s):
-    try:
-        l = ast.literal_eval(s)
-        l = [n.strip() for n in l]
-    except ValueError:
-        return 'ERROR'
+    l = ast.literal_eval(s)
     return l
