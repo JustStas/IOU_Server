c58ead770a04c17d464ea12a2d42bbe267e5a23e
Stanislav Nosulenko <stanislav.nosulenko@gmail.com>
2020-06-16 22:51:00 +0300

    Added trx creation and equal splitting

--- telegram_client.py
+++ telegram_client.py
@@ -29,7 +29,7 @@ def balance_overview(message): #todo update with a keyboard & usernames
 
     to_print = ''
 
-    counterparts = server_conn('list_users')
+    counterparts = [server_conn('list_users')]
     counterpart_usernames = counterparts
     if users == -1:
         users = counterpart_usernames
