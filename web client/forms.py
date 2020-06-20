71611f9d5623c82e014427ee0c82a17f08fdd881
Oleg Dylevich <oleg.dylevich@gmail.com>
2020-06-20 16:49:30 +0300

    working test penis web form

--- web client/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField, SubmitField
-
-class SignUpForm(FlaskForm):
-    username = StringField('Penis username')
-    password = PasswordField('Penis password')
-    submit = SubmitField('Scan your penis')
